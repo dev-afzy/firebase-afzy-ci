40e928231caf85aeb5b0ad5d4eae15ea0c3b056b
dev-afzy <dream169afi@gmail.com>
2022-11-19 13:20:26 +0530

    testing

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase 15!");
+  functions.logger.info('Hello logs!', { structuredData: true });
+  const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
+  const env = process.env.NODE_ENV;
+  response.send({ serviceAccount, env });
 });
